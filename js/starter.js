801503b260a53da2fc2b08b5ef5f7787f94f5d36
nigeljohnwade <nigeljohnwade@gmail.com>
2019-04-13 18:13:14 +0100

    Interlude: lights and materials

--- js/starter.js
+++ js/starter.js
@@ -16,32 +16,55 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const basicMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00});
-const customMaterial = new THREE.ShaderMaterial({
-    vertexShader: document.getElementById('vs').textContent.trim(),
-    fragmentShader: document.getElementById('fs').textContent.trim()
-});
-console.log(customMaterial);
-const cube1 = new THREE.Mesh(geometry, basicMaterial);
-const cube2 = new THREE.Mesh(geometry, customMaterial);
-cube1.position.x = 1;
-cube2.position.x = -1;
-scene.add( cube1 );
-scene.add( cube2 );
+const basicGreenMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00});
+const basicRedMaterial = new THREE.MeshBasicMaterial({color: 0xff0000});
+const lambertGreenMaterial = new THREE.MeshLambertMaterial({color: 0x00ff00});
+const lambertRedMaterial = new THREE.MeshLambertMaterial({color: 0xff0000});
+// const customMaterial = new THREE.ShaderMaterial({
+//     vertexShader: document.getElementById('vs').textContent.trim(),
+//     fragmentShader: document.getElementById('fs').textContent.trim()
+// });
+const lambertGreenCube = new THREE.Mesh(geometry, lambertGreenMaterial);
+const lambertRedCube = new THREE.Mesh(geometry, lambertRedMaterial);
+lambertGreenCube.position.x = 1;
+lambertRedCube.position.x = -1;
+scene.add( lambertGreenCube );
+scene.add( lambertRedCube );
+
+const basicGreenCube = new THREE.Mesh(geometry, basicGreenMaterial);
+const basicRedCube = new THREE.Mesh(geometry, basicRedMaterial);
+basicGreenCube.position.x = 0.75;
+basicRedCube.position.x = -0.75;
+scene.add( basicGreenCube );
+scene.add( basicRedCube );
+
+const light = new THREE.PointLight(0xffffff, 1, 100);
+light.position.set( 5, 5, 5 );
+scene.add( light );
 
 camera.position.z = 5;
 
 function animate(t) {
     const tDivisor = 1000;
 
-    cube1.rotation.x = t/tDivisor;
-    cube2.rotation.x = t/tDivisor;
-    cube1.rotation.y = t/tDivisor;
-    cube2.rotation.y = t/tDivisor;
-    cube1.position.x = Math.sin(t/tDivisor);
-    cube2.position.x = -Math.sin(t/tDivisor);
-    cube1.position.y = Math.cos(t/tDivisor);
-    cube2.position.y = -Math.cos(t/tDivisor);
+    lambertGreenCube.rotation.x = t/tDivisor;
+    lambertRedCube.rotation.x = t/tDivisor;
+    lambertGreenCube.rotation.y = t/tDivisor;
+    lambertRedCube.rotation.y = t/tDivisor;
+    lambertGreenCube.position.x = Math.sin(t/tDivisor);
+    lambertRedCube.position.x = -Math.sin(t/tDivisor);
+    lambertGreenCube.position.y = Math.cos(t/tDivisor);
+    lambertRedCube.position.y = -Math.cos(t/tDivisor);
+
+    basicGreenCube.rotation.x = t/tDivisor/2;
+    basicRedCube.rotation.x = t/tDivisor/2;
+    basicGreenCube.rotation.y = t/tDivisor/2;
+    basicRedCube.rotation.y = t/tDivisor/2;
+    basicGreenCube.position.x = Math.sin(t/tDivisor/2) * 2;
+    basicRedCube.position.x = -Math.sin(t/tDivisor/2) * 2;
+    basicGreenCube.position.y = Math.cos(t/tDivisor/2) * 2;
+    basicRedCube.position.y = -Math.cos(t/tDivisor/2) * 2;
+    
     renderer.render( scene, camera );
 
     requestAnimationFrame( animate );
