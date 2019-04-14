e500d227e2b93f391aa9389de2905059220af1a6
nigeljohnwade <nigeljohnwade@gmail.com>
2019-04-14 13:00:02 +0100

    Interlude: playing with geometry

--- js/starter.js
+++ js/starter.js
@@ -18,14 +18,18 @@ document.body.appendChild(renderer.domElement);
 // Geometries
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
 const sphereGeometry = new THREE.SphereGeometry(1, 32, 32);
+const tetrahedronGeometry = new THREE.TetrahedronGeometry(1, 0);
+const octahedronGeometry = new THREE.OctahedronGeometry(0.5, 0);
 
 // Materials
 const basicGreenMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00, wireframe: true});
 const basicRedMaterial = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true});
 const lambertGreenMaterial = new THREE.MeshLambertMaterial({color: 0x00ff00, transparent :true, opacity: 1});
 const lambertRedMaterial = new THREE.MeshLambertMaterial({color: 0xff0000, transparent :true, opacity: 1});
-const phongGreenMaterial = new THREE.MeshPhongMaterial({color: 0x88ff00, emissive: 0x008800, specular: 0xffffff, shininess: 300});
-const phongRedMaterial = new THREE.MeshPhongMaterial({color: 0xff8800, emissive: 0x880000, specular: 0xffffff, shininess: 300});
+const phongGreenMaterial = new THREE.MeshPhongMaterial({color: 0x00ff00, specular: 0xffffff, shininess: 300});
+const phongRedMaterial = new THREE.MeshPhongMaterial({color: 0xff0000, specular: 0xffffff, shininess: 300});
+const phongEmissiveGreenMaterial = new THREE.MeshPhongMaterial({color: 0x88ff00, emissive: 0x008800, specular: 0xffffff, shininess: 300});
+const phongEmissiveRedMaterial = new THREE.MeshPhongMaterial({color: 0xff8800, emissive: 0x880000, specular: 0xffffff, shininess: 300});
 const customGLMaterial = new THREE.ShaderMaterial({
     vertexShader: document.getElementById('vs').textContent.trim(),
     fragmentShader: document.getElementById('fs').textContent.trim()
@@ -39,12 +43,19 @@ lambertRedCube.position.x = -1;
 scene.add( lambertGreenCube );
 scene.add( lambertRedCube );
 
-const phongGreenCube = new THREE.Mesh(boxGeometry, phongGreenMaterial);
-const phongRedCube = new THREE.Mesh(boxGeometry, phongRedMaterial);
-phongGreenCube.position.x = 0.5;
-phongRedCube.position.x = -0.5;
-scene.add( phongGreenCube );
-scene.add( phongRedCube );
+const phongGreenOctahedron = new THREE.Mesh(octahedronGeometry, phongGreenMaterial);
+const phongRedOctahedron = new THREE.Mesh(octahedronGeometry, phongRedMaterial);
+phongGreenOctahedron.position.x = 0.5;
+phongRedOctahedron.position.x = -0.5;
+scene.add( phongGreenOctahedron );
+scene.add( phongRedOctahedron );
+
+const phongEmissiveGreenTetrahedron = new THREE.Mesh(tetrahedronGeometry, phongEmissiveGreenMaterial);
+const phongEmissiveRedTetrahedron = new THREE.Mesh(tetrahedronGeometry, phongEmissiveRedMaterial);
+phongEmissiveGreenTetrahedron.position.x = 0.5;
+phongEmissiveRedTetrahedron.position.x = -0.5;
+scene.add( phongEmissiveGreenTetrahedron );
+scene.add( phongEmissiveRedTetrahedron );
 
 const basicGreenCube = new THREE.Mesh(boxGeometry, basicGreenMaterial);
 const basicRedCube = new THREE.Mesh(boxGeometry, basicRedMaterial);
@@ -64,7 +75,7 @@ scene.add(directionalSunLight);
 const originPointLight = new THREE.PointLight(0xffffff, 1, 20);
 originPointLight.position.set( 0, 0, 0);
 scene.add( originPointLight );
-const rotatingPointLight = new THREE.PointLight(0xffff00, 0.6, 200);
+const rotatingPointLight = new THREE.PointLight(0xffffff, 0.6, 200);
 rotatingPointLight.position.set( 0, 0, 0);
 scene.add( rotatingPointLight );
 camera.position.z = 5;
@@ -87,20 +98,31 @@ function animate(t) {
     lambertGreenCube.position.z = -Math.cos(t/tDivisor);
     lambertRedCube.position.z = Math.cos(t/tDivisor);
 
-    phongGreenCube.scale.y = Math.sin(t/tDivisor) +1;
-    phongRedCube.scale.y = Math.cos(t/tDivisor) +1;
-    phongGreenCube.rotation.x = -t/tDivisor;
-    phongRedCube.rotation.x = -t/tDivisor;
-    phongGreenCube.rotation.y = -t/tDivisor;
-    phongRedCube.rotation.y = -t/tDivisor;
-    phongGreenCube.rotation.z = -t/tDivisor;
-    phongRedCube.rotation.z = -t/tDivisor;
-    phongGreenCube.position.x = Math.sin(-t/tDivisor/0.5) * 0.5;
-    phongRedCube.position.x = -Math.sin(-t/tDivisor/0.5) * 0.5;
-    phongGreenCube.position.z = Math.sin(-t/tDivisor/0.5) * 0.5;
-    phongRedCube.position.z = -Math.sin(-t/tDivisor/0.5) * 0.5;
-    phongGreenCube.position.y = Math.cos(-t/tDivisor/0.5) * 0.5;
-    phongRedCube.position.y = -Math.cos(-t/tDivisor/0.5) * 0.5;
+    phongGreenOctahedron.rotation.x = -t/tDivisor;
+    phongRedOctahedron.rotation.x = -t/tDivisor;
+    phongGreenOctahedron.rotation.y = -t/tDivisor;
+    phongRedOctahedron.rotation.y = -t/tDivisor;
+    phongGreenOctahedron.rotation.z = -t/tDivisor;
+    phongRedOctahedron.rotation.z = -t/tDivisor;
+    phongGreenOctahedron.position.x = Math.sin(-t/tDivisor/0.5) * 0.5;
+    phongRedOctahedron.position.x = -Math.sin(-t/tDivisor/0.5) * 0.5;
+    phongGreenOctahedron.position.z = Math.sin(-t/tDivisor/0.5) * 0.5;
+    phongRedOctahedron.position.z = -Math.sin(-t/tDivisor/0.5) * 0.5;
+    phongGreenOctahedron.position.y = Math.cos(-t/tDivisor/0.5) * 0.5;
+    phongRedOctahedron.position.y = -Math.cos(-t/tDivisor/0.5) * 0.5;
+
+    phongEmissiveGreenTetrahedron.rotation.x = -t/tDivisor/0.2;
+    phongEmissiveRedTetrahedron.rotation.x = t/tDivisor/0.2;
+    phongEmissiveGreenTetrahedron.rotation.y = t/tDivisor/0.1;
+    phongEmissiveRedTetrahedron.rotation.y = -t/tDivisor/0.1;
+    phongEmissiveGreenTetrahedron.rotation.z = -t/tDivisor/0.3;
+    phongEmissiveRedTetrahedron.rotation.z = t/tDivisor/0.3;
+    phongEmissiveGreenTetrahedron.position.x = -Math.sin(t/tDivisor/0.5) * 3;
+    phongEmissiveRedTetrahedron.position.x = -Math.sin(-t/tDivisor/0.5) * 3;
+    phongEmissiveGreenTetrahedron.position.z = -Math.sin(t/tDivisor/0.5) * 3;
+    phongEmissiveRedTetrahedron.position.z = -Math.sin(-t/tDivisor/0.5) * 3;
+    phongEmissiveGreenTetrahedron.position.y = -Math.cos(t/tDivisor/0.5) * 3;
+    phongEmissiveRedTetrahedron.position.y = Math.cos(-t/tDivisor/0.5) * 3;
 
 
     basicGreenCube.scale.z = Math.sin(t/tDivisor) +1;
@@ -121,12 +143,13 @@ function animate(t) {
     customBlueSphere.scale.x = Math.sin(t/tDivisor/4);
     customBlueSphere.scale.y = Math.sin(t/tDivisor/4);
     customBlueSphere.scale.z = Math.sin(t/tDivisor/4);
-    renderer.render( scene, camera );
 
     originPointLight.intensity = Math.sin(t/tDivisor/0.2);
     rotatingPointLight.position.x = Math.sin(t/tDivisor/5) * 5;
     rotatingPointLight.position.y = Math.cos(t/tDivisor/5) * 5;
     rotatingPointLight.position.z = Math.sin(t/tDivisor/5) * 5;
+
+    renderer.render( scene, camera );
     requestAnimationFrame( animate );
 }
 if ( WEBGL.isWebGLAvailable() ) {
