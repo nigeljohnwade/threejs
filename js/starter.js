7d65aca0544d694b640a7e8047af8cda18c1738f
nigeljohnwade <nigeljohnwade@gmail.com>
2019-04-14 01:15:49 +0100

    Interlude: playing with materials

--- js/starter.js
+++ js/starter.js
@@ -16,10 +16,12 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const basicGreenMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00});
-const basicRedMaterial = new THREE.MeshBasicMaterial({color: 0xff0000});
-const lambertGreenMaterial = new THREE.MeshLambertMaterial({color: 0x00ff00});
-const lambertRedMaterial = new THREE.MeshLambertMaterial({color: 0xff0000});
+const basicGreenMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00, wireframe: true});
+const basicRedMaterial = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true});
+const lambertGreenMaterial = new THREE.MeshLambertMaterial({color: 0x00ff00, transparent :true, opacity: 0.5});
+const lambertRedMaterial = new THREE.MeshLambertMaterial({color: 0xff0000, transparent :true, opacity: 0.5});
+const phongGreenMaterial = new THREE.MeshPhongMaterial({color: 0x88ff00, emissive: 0x008800, specular: 0xffffff, shininess: 300});
+const phongRedMaterial = new THREE.MeshPhongMaterial({color: 0xff8800, emissive: 0x880000, specular: 0xffffff, shininess: 300});
 // const customMaterial = new THREE.ShaderMaterial({
 //     vertexShader: document.getElementById('vs').textContent.trim(),
 //     fragmentShader: document.getElementById('fs').textContent.trim()
@@ -30,6 +32,13 @@ lambertGreenCube.position.x = 1;
 lambertRedCube.position.x = -1;
 scene.add( lambertGreenCube );
 scene.add( lambertRedCube );
+
+const phongGreenCube = new THREE.Mesh(geometry, phongGreenMaterial);
+const phongRedCube = new THREE.Mesh(geometry, phongRedMaterial);
+phongGreenCube.position.x = 0.5;
+phongRedCube.position.x = -0.5;
+scene.add( phongGreenCube );
+scene.add( phongRedCube );
 
 const basicGreenCube = new THREE.Mesh(geometry, basicGreenMaterial);
 const basicRedCube = new THREE.Mesh(geometry, basicRedMaterial);
@@ -47,23 +56,51 @@ camera.position.z = 5;
 function animate(t) {
     const tDivisor = 1000;
 
-    lambertGreenCube.rotation.x = t/tDivisor;
-    lambertRedCube.rotation.x = t/tDivisor;
+    lambertGreenCube.scale.x = Math.sin(t/tDivisor) +1;
+    lambertRedCube.scale.x = Math.cos(t/tDivisor) +1;
+    lambertGreenCube.rotation.x = t/tDivisor/0.5;
+    lambertRedCube.rotation.x = t/tDivisor/0.5;
     lambertGreenCube.rotation.y = t/tDivisor;
     lambertRedCube.rotation.y = t/tDivisor;
-    lambertGreenCube.position.x = Math.sin(t/tDivisor);
-    lambertRedCube.position.x = -Math.sin(t/tDivisor);
-    lambertGreenCube.position.y = Math.cos(t/tDivisor);
-    lambertRedCube.position.y = -Math.cos(t/tDivisor);
+    lambertGreenCube.rotation.z = t/tDivisor;
+    lambertRedCube.rotation.z = t/tDivisor;
+    lambertGreenCube.position.x = -Math.sin(t/tDivisor);
+    lambertRedCube.position.x = Math.sin(t/tDivisor);
+    lambertGreenCube.position.y = -Math.cos(t/tDivisor);
+    lambertRedCube.position.y = Math.cos(t/tDivisor);
+    lambertGreenCube.position.z = -Math.cos(t/tDivisor);
+    lambertRedCube.position.z = Math.cos(t/tDivisor);
 
-    basicGreenCube.rotation.x = t/tDivisor/2;
+    phongGreenCube.scale.y = Math.sin(t/tDivisor) +1;
+    phongRedCube.scale.y = Math.cos(t/tDivisor) +1;
+    phongGreenCube.rotation.x = -t/tDivisor;
+    phongRedCube.rotation.x = -t/tDivisor;
+    phongGreenCube.rotation.y = -t/tDivisor;
+    phongRedCube.rotation.y = -t/tDivisor;
+    phongGreenCube.rotation.z = -t/tDivisor;
+    phongRedCube.rotation.z = -t/tDivisor;
+    phongGreenCube.position.x = Math.sin(-t/tDivisor/0.5) * 0.5;
+    phongRedCube.position.x = -Math.sin(-t/tDivisor/0.5) * 0.5;
+    phongGreenCube.position.z = Math.sin(-t/tDivisor/0.5) * 0.5;
+    phongRedCube.position.z = -Math.sin(-t/tDivisor/0.5) * 0.5;
+    phongGreenCube.position.y = Math.cos(-t/tDivisor/0.5) * 0.5;
+    phongRedCube.position.y = -Math.cos(-t/tDivisor/0.5) * 0.5;
+
+
+    basicGreenCube.scale.z = Math.sin(t/tDivisor) +1;
+    basicRedCube.scale.z = Math.cos(t/tDivisor) +1;
+    basicGreenCube.rotation.x = -t/tDivisor/2;
     basicRedCube.rotation.x = t/tDivisor/2;
     basicGreenCube.rotation.y = t/tDivisor/2;
-    basicRedCube.rotation.y = t/tDivisor/2;
+    basicRedCube.rotation.y = -t/tDivisor/2;
+    basicGreenCube.rotation.z = t/tDivisor/0.2;
+    basicRedCube.rotation.z = t/tDivisor/0.2;
     basicGreenCube.position.x = Math.sin(t/tDivisor/2) * 2;
     basicRedCube.position.x = -Math.sin(t/tDivisor/2) * 2;
     basicGreenCube.position.y = Math.cos(t/tDivisor/2) * 2;
     basicRedCube.position.y = -Math.cos(t/tDivisor/2) * 2;
+    basicGreenCube.position.z = -Math.cos(t/tDivisor/2) * 2;
+    basicRedCube.position.z = -Math.cos(t/tDivisor/2) * 2;
     
     renderer.render( scene, camera );
 
