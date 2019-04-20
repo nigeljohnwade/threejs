663768b17331bfd267aef1c91964b5832b97f989
nigeljohnwade <nigeljohnwade@gmail.com>
2019-04-20 10:56:11 +0100

    Refactor

--- js/starter.js
+++ js/starter.js
@@ -17,7 +17,6 @@ document.body.appendChild(renderer.domElement);
 
 // Geometries
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
-const sphereGeometry = new THREE.SphereGeometry(1, 32, 32);
 const tetrahedronGeometry = new THREE.TetrahedronGeometry(1, 0);
 const octahedronGeometry = new THREE.OctahedronGeometry(0.5, 0);
 
@@ -39,10 +38,6 @@ const phongEmissiveRedMaterial = new THREE.MeshPhongMaterial({
     emissive: 0x880000,
     specular: 0xffffff,
     shininess: 300
-});
-const customGLMaterial = new THREE.ShaderMaterial({
-    vertexShader: document.getElementById('vs').textContent.trim(),
-    fragmentShader: document.getElementById('fs').textContent.trim()
 });
 
 // Objects
@@ -74,9 +69,6 @@ basicRedCube.position.x = -0.75;
 scene.add(basicGreenCube);
 scene.add(basicRedCube);
 
-const customBlueSphere = new THREE.Mesh(sphereGeometry, customGLMaterial);
-// scene.add( customBlueSphere );
-
 // Lights
 const softAmbientLight = new THREE.AmbientLight(0x030303);
 scene.add(softAmbientLight);
@@ -105,65 +97,81 @@ function animate(t) {
     const tDivisor = 1000;
 
     lambertGreenCube.scale.x = Math.sin(t / tDivisor) + 1;
+    lambertGreenCube.rotation.set(
+        t / tDivisor / 0.5,
+        t / tDivisor,
+        t / tDivisor);
+    lambertGreenCube.position.set(
+        -Math.sin(t / tDivisor),
+        -Math.cos(t / tDivisor),
+        -Math.cos(t / tDivisor));
+
     lambertRedCube.scale.x = Math.cos(t / tDivisor) + 1;
-    lambertGreenCube.rotation.x = t / tDivisor / 0.5;
-    lambertRedCube.rotation.x = t / tDivisor / 0.5;
-    lambertGreenCube.rotation.y = t / tDivisor;
-    lambertRedCube.rotation.y = t / tDivisor;
-    lambertGreenCube.rotation.z = t / tDivisor;
-    lambertRedCube.rotation.z = t / tDivisor;
-    lambertGreenCube.position.x = -Math.sin(t / tDivisor);
-    lambertRedCube.position.x = Math.sin(t / tDivisor);
-    lambertGreenCube.position.y = -Math.cos(t / tDivisor);
-    lambertRedCube.position.y = Math.cos(t / tDivisor);
-    lambertGreenCube.position.z = -Math.cos(t / tDivisor);
-    lambertRedCube.position.z = Math.cos(t / tDivisor);
+    lambertRedCube.rotation.set(
+        t / tDivisor / 0.5,
+        t / tDivisor,
+        t / tDivisor);
+    lambertRedCube.position.set(
+        Math.sin(t / tDivisor),
+        Math.cos(t / tDivisor),
+        Math.cos(t / tDivisor));
 
-    phongGreenOctahedron.rotation.x = -t / tDivisor;
-    phongRedOctahedron.rotation.x = -t / tDivisor;
-    phongGreenOctahedron.rotation.y = -t / tDivisor;
-    phongRedOctahedron.rotation.y = -t / tDivisor;
-    phongGreenOctahedron.rotation.z = -t / tDivisor;
-    phongRedOctahedron.rotation.z = -t / tDivisor;
-    phongGreenOctahedron.position.x = Math.sin(-t / tDivisor / 0.5) * 0.5;
-    phongRedOctahedron.position.x = -Math.sin(-t / tDivisor / 0.5) * 0.5;
-    phongGreenOctahedron.position.z = Math.sin(-t / tDivisor / 0.5) * 0.5;
-    phongRedOctahedron.position.z = -Math.sin(-t / tDivisor / 0.5) * 0.5;
-    phongGreenOctahedron.position.y = Math.cos(-t / tDivisor / 0.5) * 0.5;
-    phongRedOctahedron.position.y = -Math.cos(-t / tDivisor / 0.5) * 0.5;
+    phongGreenOctahedron.rotation.set(
+        -t / tDivisor,
+        -t / tDivisor,
+        -t / tDivisor);
+    phongGreenOctahedron.position.set(
+        Math.sin(-t / tDivisor / 0.5) * 0.5,
+        Math.sin(-t / tDivisor / 0.5) * 0.5,
+        Math.cos(-t / tDivisor / 0.5) * 0.5);
 
-    phongEmissiveGreenTetrahedron.rotation.x = -t / tDivisor / 0.2;
-    phongEmissiveRedTetrahedron.rotation.x = t / tDivisor / 0.2;
-    phongEmissiveGreenTetrahedron.rotation.y = t / tDivisor / 0.1;
-    phongEmissiveRedTetrahedron.rotation.y = -t / tDivisor / 0.1;
-    phongEmissiveGreenTetrahedron.rotation.z = -t / tDivisor / 0.3;
-    phongEmissiveRedTetrahedron.rotation.z = t / tDivisor / 0.3;
-    phongEmissiveGreenTetrahedron.position.x = -Math.sin(t / tDivisor / 0.5) * 3;
-    phongEmissiveRedTetrahedron.position.x = -Math.sin(-t / tDivisor / 0.5) * 3;
-    phongEmissiveGreenTetrahedron.position.z = -Math.sin(t / tDivisor / 0.5) * 3;
-    phongEmissiveRedTetrahedron.position.z = -Math.sin(-t / tDivisor / 0.5) * 3;
-    phongEmissiveGreenTetrahedron.position.y = -Math.cos(t / tDivisor / 0.5) * 3;
-    phongEmissiveRedTetrahedron.position.y = Math.cos(-t / tDivisor / 0.5) * 3;
+    phongRedOctahedron.rotation.set(
+        -t / tDivisor,
+        -t / tDivisor,
+        -t / tDivisor);
+    phongRedOctahedron.position.set(
+        -Math.sin(-t / tDivisor / 0.5) * 0.5,
+        -Math.sin(-t / tDivisor / 0.5) * 0.5,
+        -Math.cos(-t / tDivisor / 0.5) * 0.5);
+
+    phongEmissiveGreenTetrahedron.rotation.set(
+        -t / tDivisor / 0.2,
+        t / tDivisor / 0.1,
+        -t / tDivisor / 0.3);
+    phongEmissiveGreenTetrahedron.position.set(
+        -Math.sin(t / tDivisor / 0.5) * 3,
+        -Math.sin(t / tDivisor / 0.5) * 3,
+        -Math.cos(t / tDivisor / 0.5) * 3);
+
+    phongEmissiveRedTetrahedron.rotation.set(
+        t / tDivisor / 0.2,
+        -t / tDivisor / 0.1,
+        t / tDivisor / 0.3);
+    phongEmissiveRedTetrahedron.position.set(
+        -Math.sin(-t / tDivisor / 0.5) * 3,
+        -Math.sin(-t / tDivisor / 0.5) * 3,
+        Math.cos(-t / tDivisor / 0.5) * 3);
 
 
     basicGreenCube.scale.z = Math.sin(t / tDivisor) + 1;
-    basicRedCube.scale.z = Math.cos(t / tDivisor) + 1;
-    basicGreenCube.rotation.x = -t / tDivisor / 2;
-    basicRedCube.rotation.x = t / tDivisor / 2;
-    basicGreenCube.rotation.y = t / tDivisor / 2;
-    basicRedCube.rotation.y = -t / tDivisor / 2;
-    basicGreenCube.rotation.z = t / tDivisor / 0.2;
-    basicRedCube.rotation.z = t / tDivisor / 0.2;
-    basicGreenCube.position.x = Math.sin(t / tDivisor / 2) * 2;
-    basicRedCube.position.x = -Math.sin(t / tDivisor / 2) * 2;
-    basicGreenCube.position.y = Math.cos(t / tDivisor / 2) * 2;
-    basicRedCube.position.y = -Math.cos(t / tDivisor / 2) * 2;
-    basicGreenCube.position.z = -Math.cos(t / tDivisor / 2) * 2;
-    basicRedCube.position.z = -Math.cos(t / tDivisor / 2) * 2;
+    basicGreenCube.rotation.set(
+        -t / tDivisor / 2,
+        t / tDivisor / 2,
+        t / tDivisor / 0.2);
+    basicGreenCube.position.set(
+        Math.sin(t / tDivisor / 2) * 2,
+        Math.cos(t / tDivisor / 2) * 2,
+        -Math.cos(t / tDivisor / 2) * 2);
 
-    customBlueSphere.scale.x = Math.sin(t / tDivisor / 4);
-    customBlueSphere.scale.y = Math.sin(t / tDivisor / 4);
-    customBlueSphere.scale.z = Math.sin(t / tDivisor / 4);
+    basicRedCube.scale.z = Math.cos(t / tDivisor) + 1;
+    basicRedCube.rotation.set(
+        t / tDivisor / 2,
+        -t / tDivisor / 2,
+        t / tDivisor / 0.2);
+    basicRedCube.position.set(
+        -Math.sin(t / tDivisor / 2) * 2,
+        -Math.cos(t / tDivisor / 2) * 2,
+        -Math.cos(t / tDivisor / 2) * 2);
 
     originPointLight.intensity = Math.sin(t / tDivisor / 0.2);
     rotatingPointLight.position.x = Math.sin(t / tDivisor / 5) * 5;
